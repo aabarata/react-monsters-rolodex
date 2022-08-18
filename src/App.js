8c9baaf9f88331df8c8ce3e36adb62a1b2a1701a
aabarata <andrebarata.eu@gmail.com>
2022-08-18 11:26:44 +0100

    [Basics]: iterate list

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
 import { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: "Andre",
+      monsters: [
+        { id: 0, name: "Linda" },
+        { id: 1, name: "Frank" },
+        { id: 2, name: "Jacky" },
+        { id: 3, name: "Andrei" },
+      ],
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  return { name: "Barata" };
-                },
-                () => {
-                  // Only executed when the state is updated
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-            Change name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
