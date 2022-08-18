2f481632f3732ce03c84fae9768708ec8d4537b8
aabarata <andrebarata.eu@gmail.com>
2022-08-18 14:47:37 +0100

    [Basics]: search input + optimizations

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -13,14 +14,35 @@ class App extends Component {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const responseUsers = await response.json();
     this.setState(() => {
-      return { monsters: responseUsers };
+      return { monsters: responseUsers, filteredMonstersList: responseUsers };
     });
   }
 
+  onSearchChange = (event) => {
+    this.setState(() => {
+      return {
+        searchField: event.target.value.toLowerCase(),
+      };
+    });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField)
+    );
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
