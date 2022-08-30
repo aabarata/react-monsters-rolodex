5ae404d1a3200982dea958237e536f8f63ef02b3
aabarata <andrebarata.eu@gmail.com>
2022-08-30 18:58:55 +0200

    [Hooks]: convert App.js to funcional component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,51 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import SearchBox from "./components/searchBox/searchBox.component";
 import CardList from "./components/cardList/cardList.component";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: "",
+const App = () => {
+  //Create the get/set and initialize searchField as an empty string
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  //The condition array will be empty since this effect will only be runned one time (first render)
+  //// If we wanted to run again based in a variable we passed into the conditional array
+  useEffect(() => {
+    return async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const responseUsers = await response.json();
+      setMonsters(responseUsers);
     };
-  }
+  }, []);
 
-  async componentDidMount() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const responseUsers = await response.json();
-    this.setState(() => {
-      return { monsters: responseUsers, filteredMonstersList: responseUsers };
-    });
-  }
-
-  onSearchChange = (event) => {
-    this.setState(() => {
-      return {
-        searchField: event.target.value.toLowerCase(),
-      };
-    });
-  };
-
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) =>
+  //The condition array includes the searchField state field since will be re-runned everytime it's changed
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchField)
     );
+    setFilteredMonsters(newFilteredMonsters);
+  }, [searchField, monsters]);
 
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
+  };
 
-        <SearchBox
-          className="monsters-search-box"
-          placeholder="search monsters"
-          onChangeHandler={onSearchChange}
-        />
-        <CardList list={filteredMonsters} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+
+      <SearchBox
+        className="monsters-search-box"
+        placeholder="search monsters"
+        onChangeHandler={onSearchChange}
+      />
+      <CardList list={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
